1c865c40d3494af30b090807d5c2fe0439d2f964
Berina Sanjta <berina@sanjta.org>
2021-09-21 12:19:37 +0200

    Adding folder ex02 and changes to folder ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,5 +1,7 @@
 // Only change code below this line
-var myTrueBoolean = true;
-console.log(myTrueBoolean);
+function myTrueBoolean (){
+    return true;
+};
+console.log(myTrueBoolean());
 // Only change code above this line
 module.exports = myTrueBoolean;
